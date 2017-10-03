9a29feab412ed6b5cbe2ffc5edb0c94b58dfb270
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-03 19:35:33 -0400

    first commit for people assignment

---
+++ people.rb
@@ -0,0 +1,15 @@
+# Let's start by creating two classes: one called Student and another called Instructor.
+Class instructor
+
+end
+
+
+
+Class student
+
+# The student class has a method called learn that returns "I get it!".
+def learn
+  puts "I Get it!"
+end
+
+end
